3777ef44b6c7f76219dc640a67287430d34ae2a3
Hazem Hassan <zamazoka@gmail.com>
2025-04-29 16:42:26 +0300

    Add identity to the project

---
+++ School.Data/Entities/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace School.Data.Entities.IdentityEntities
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+
+    }
+}
