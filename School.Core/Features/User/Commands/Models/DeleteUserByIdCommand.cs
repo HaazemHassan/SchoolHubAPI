21906295bba51cd3aa90990780f7134fa5b78b59
Hazem Hassan <zamazoka@gmail.com>
2025-05-01 17:40:57 +0300

    Implement delete user endpoint.

---
+++ School.Core/Features/User/Commands/Models/DeleteUserByIdCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using School.Core.Bases;
+
+namespace School.Core.Features.User.Commands.Models
+{
+    public class DeleteUserByIdCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+
+    }
+}
