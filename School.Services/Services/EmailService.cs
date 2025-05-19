9f780fa19a04f5a29197d7126b810353adc2ddeb
Hazem Hassan <zamazoka@gmail.com>
2025-05-19 16:58:18 +0300

    Implement send email endpoint.

---
+++ School.Services/Services/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using School.Data.Helpers.Email;
+using School.Services.ServicesContracts;
+
+namespace School.Services.Services
+{
+    public class EmailService : IEmailService
+    {
+
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(EmailSettings emailSettings)
+        {
+            _emailSettings = emailSettings;
+        }
+        public async Task<bool> SendEmail(string email, string Message)
+        {
+            try
+            {
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(_emailSettings.Host, _emailSettings.Port, true);
+                    await client.AuthenticateAsync(_emailSettings.SenderEmail, _emailSettings.Password);
+                    var bodybuilder = new BodyBuilder
+                    {
+                        HtmlBody = $"{Message}",
+                        TextBody = "wellcome",
+                    };
+                    var message = new MimeMessage
+                    {
+                        Body = bodybuilder.ToMessageBody()
+                    };
+                    message.From.Add(new MailboxAddress("SchoohAPI", _emailSettings.SenderEmail));
+                    message.To.Add(new MailboxAddress("testing", email));
+                    message.Subject = "new Contact Submitted Data";
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
